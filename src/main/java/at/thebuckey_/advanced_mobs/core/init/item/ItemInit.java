c5bbe886c9d1fc0d3ab477bd5fa0903031e4131f
TheBuckey <buchsteiner.felix@gmail.com>
2021-12-23 17:01:53 +0100

    Added Power Tomato whit Speed effect.

--- src/main/java/at/thebuckey_/advanced_mobs/core/init/item/ItemInit.java
+++ src/main/java/at/thebuckey_/advanced_mobs/core/init/item/ItemInit.java
@@ -2,11 +2,18 @@ package at.thebuckey_.advanced_mobs.core.init.item;
 
 import at.thebuckey_.advanced_mobs.Advanced_mobs;
 import at.thebuckey_.advanced_mobs.core.init.ModCreativeModeTab;
+import net.minecraft.client.renderer.EffectInstance;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+
+import java.io.IOException;
 
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS =
@@ -20,9 +27,25 @@ public class ItemInit {
     // Raw Titanium
     public static final RegistryObject<Item> RAW_TITANIUM = ITEMS.register("raw_titanium",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.ADVANCED_MOBS_MISC)));
+    //Power Tomato
+    public static final RegistryObject<Item> POWER_TOMATO = ITEMS.register("power_tomato", () -> new Item(
+            new Item.Properties()
+                    .tab(ModCreativeModeTab.ADVANCED_MOBS)
+                    .food(new FoodProperties.Builder()
+                            .nutrition(2)
+                            .saturationMod(1f)
+                            .alwaysEat()
+                            .fast()
+                            .effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 25), 1)
+                            .build()
+                    )
+    ));
+
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
+
 
 
 }
